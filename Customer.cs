6504c1c0d6537b6416d36a40cee2527d72355e6d
Thomas Lauer <tjlauer36579@gmail.com>
2021-09-28 18:22:30 -0500

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,99 @@
+﻿// Thomas Lauer
+// MavPASS 20210914
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_20210914
+{
+    public class Customer
+    {
+        //* Variables
+        private string customerID = "n/a";
+        private string name = "n/a";
+        private string phoneNumber = "n/a";
+        private string address = "n/a";
+        private string plan = "n/a";
+        private List<string> members = new List<string>();
+        private List<string> memberNumbers = new List<string>();
+        //*/
+
+        //* Properties
+        public string CustomerID {
+            get { return this.customerID; }
+            set {
+                this.customerID = value;
+            }
+        }
+        public string Name { get { return this.name; } set { this.name = value; } }
+        public string PhoneNumber { get { return this.phoneNumber; } set { this.phoneNumber = value; } }
+        public string Address { get { return this.address; } set { this.address = value; } }
+        public string Plan { get { return this.plan; } set { this.plan = value; } }
+        public List<string> Members { get { return this.members; } set { this.members = value; } }
+        public List<string> MemberNumbers { get { return this.memberNumbers; } set { this.memberNumbers = value; } }
+
+        
+        //*/
+
+        // Constructors
+        public Customer() { /* empty */ }
+
+        public Customer(string aCustomerID, string aName, string aPhoneNumber, string anAddress, string aPlan, List<string> aMembers, List<string> aMemberNumbers)
+        {
+            this.CustomerID = aCustomerID;
+            this.Name = aName;
+            this.PhoneNumber = aPhoneNumber;
+            this.Address = anAddress;
+            this.Plan = aPlan;
+            this.Members = aMembers;
+            this.MemberNumbers = aMemberNumbers;
+        }
+
+        // Methods
+        public void CheckValidity()
+        {
+            if (Members.Count > MemberNumbers.Count)
+            {
+                int difference = Members.Count - MemberNumbers.Count;
+                string unknown = "n/a";
+
+                for(int i=0; i < difference ;i++)
+                {
+                    MemberNumbers.Add(unknown);
+                }
+
+
+            }
+            else if(MemberNumbers.Count > Members.Count)
+            {
+                int difference = MemberNumbers.Count - Members.Count;
+                string unknown = "n/a";
+
+                for (int i=0; i < difference ;i++)
+                {
+                    Members.Add(unknown);
+                }
+
+            }
+        }
+
+
+        public override string ToString()
+        {
+            string msg = "";
+            msg += "CustomerID: " + this.CustomerID + "\n";
+            msg += "Name: " + this.Name + "\n";
+            msg += "PhoneNumber: " + this.PhoneNumber + "\n";
+            msg += "Address: " + this.Address + "\n";
+            msg += "Plan: " + this.Plan + "\n";
+            msg += "Members:\n";
+            foreach(string member in this.Members) { msg += "\t" + member + "\n"; }
+            msg += "MemberNumbers:\n";
+            foreach (string memberNumber in this.MemberNumbers) { msg += "\t" + memberNumber + "\n"; }
+            return msg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿// Thomas Lauer
+// MavPASS 20210914
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MavPASS_20210914
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            // Thomas
+            Customer myCustomer1 = new Customer(
+                "TheID",
+                "Joe Shmoe III",
+                "3005884300",
+                "Washington DC, Florida",
+                "FamilyPlan",
+                new List<string>() { "Name1" },
+                new List<string>() { "PhoneNumber1", "PhoneNumber2" }
+                );
+
+            // Madelyn
+            Customer myCustomer2 = new Customer(
+                "55,444,555,555,0,6,33,0,66,666,9",
+                "Ham Stu",
+                "800-273-8255",
+                "420 Punk Ave, Denver Oregon",
+                "Single Plan",
+                new List<string>() { },
+                new List<string>() { }
+                );
+
+            // Kidus
+            Customer myCustomer3 = new Customer(
+                "The id",
+                "Kidus Alex",
+                "240-476-9786",
+                "Mankato MN",
+                "Life plan",
+                new List<string>() { "Kidus", "Geni", "Wengel", "Alex" },
+                new List<string>() { "244", "655" }
+                );
+
+            // Nathaniel
+            Customer myCustomer4 = new Customer(
+                "4815162342",
+                "Donald Biden",
+                "651-488-8888",
+                "666 Emo Ave, LA Florida", 
+                "Family Plan",
+                new List<string>(){"Obama","Joe","Washington","Lincoln"},
+                new List<string>(){"911","411","N/A","311"}
+            );
+
+            //Simon
+
+            Customer myCustomer5 = new Customer(
+                "45656374",
+                "Bob Smith",
+                "098-776-5874",
+                "557 Jude Ave, MD Baltimore",
+                "Family Plan",
+                new List<string>() { "simon", "kidus", "meri"},
+                new List<string>() {"8897", "4455", "3232"}
+                
+            );      
+
+            //Sungjae
+            Customer myCustomer6 = new Customer(
+                "00002351",
+                "Sungjae Oh",
+                "6123052235",
+                "Atlanta, Georgia",
+                "Super Premium Plan",
+                new List<string>() {"Donald Oh"},
+                new List<string>() {"7757790153"}
+                
+            );
+
+            // ANSWER IS BELOW THIS LINE         
+
+            myCustomer1.CheckValidity();
+            myCustomer2.CheckValidity();
+            myCustomer3.CheckValidity();
+            myCustomer4.CheckValidity();
+            myCustomer5.CheckValidity();
+            myCustomer6.CheckValidity();
+
+            customers.Add(myCustomer1);
+            customers.Add(myCustomer2);
+            customers.Add(myCustomer3);
+            customers.Add(myCustomer4);
+            customers.Add(myCustomer5);
+            customers.Add(myCustomer6);
+
+            
+            // ANSWER IS ABOVE THIS LINE
+
+
+            // waste of computation time
+
+
+            
+
+            foreach(Customer c in customers)
+            {
+               Console.WriteLine(c.ToString()+"\n");
+                
+                
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
